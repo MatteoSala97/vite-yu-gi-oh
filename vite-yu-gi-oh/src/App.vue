550ea917298d527666054c4e64ece71243a7c338
MatteoSala97 <matteo111997@gmail.com>
2024-02-09 14:54:32 +0100

    config

---
+++ vite-yu-gi-oh/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import HeaderApp from './components/HeaderApp.vue';
+
+export default{
+  components:{
+    HeaderApp,
+  }
+}
+
+</script>
+
+<template>
+  <h1 class="text-danger">AAAAAAAAAAAAAAAAAAAAAAARGH!</h1>
+  <HeaderApp/>
+</template>
+
+<style>
+
+</style>
---
+++ vite-yu-gi-oh/src/components/HeaderApp.vue
@@ -0,0 +1,16 @@
+<script>
+
+
+export default{
+
+}
+
+</script>
+
+<template>
+  <h1 class="text-primary "> AHAHAHAHAH!</h1>
+</template>
+
+<style>
+
+</style>

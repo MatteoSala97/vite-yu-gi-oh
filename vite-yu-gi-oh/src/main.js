4f77c545832308704e6901a80685300611682d13
MatteoSala97 <matteo111997@gmail.com>
2024-02-09 15:10:03 +0100

    config

--- vite-yu-gi-oh/src/main.js
+++ vite-yu-gi-oh/src/main.js
@@ -1,7 +1,10 @@
+// Import our custom CSS
+import './scss/styles.scss'
 
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
